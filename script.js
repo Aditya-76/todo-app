0889c571c5d2c10e231c1dd037b699b4e9e25044
Aditya Shekhar <aditya7682@gmail.com>
2023-09-27 01:07:15 +0530

    Make todo App

---
+++ script.js
@@ -0,0 +1,65 @@
+document.addEventListener("DOMContentLoaded", webscript);
+
+function webscript() {
+  const currTime = new Date().toLocaleDateString("en-us", {
+    weekday: "long",
+    year: "numeric",
+    month: "short",
+    day: "numeric"
+  });
+
+  //add time to dom
+  document.getElementById("time").innerHTML = currTime;
+
+  //intial render function
+  function init() {
+    displayList();
+  }
+  init();
+
+  //add evnet listener to + button (using form)
+  const form = document.querySelector("#todo-data");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    document.querySelector("#todo-list").innerHTML = "";
+    const todo = document.getElementById("input");
+    addToLocal(todo.value, todo.value);
+    todo.value = "";
+    displayList();
+  });
+
+  //add to localStorage
+  function addToLocal(key, value) {
+    localStorage.setItem(key, value);
+  }
+
+  //clear localStorage
+  function clearAll() {
+    localStorage.clear();
+    document.querySelector("#todo-list").innerHTML = "";
+  }
+  //clear button
+  const clearButton = document.getElementById("clear");
+  clearButton.addEventListener("click", () => clearAll());
+
+  //function to display list using localStorage
+  function displayList() {
+    for (let i = 0; i < localStorage.length; i++) {
+      addToList(localStorage.getItem(localStorage.key(i)));
+    }
+  }
+
+  //add to DOM
+  function addToList(todo) {
+    const ulEle = document.getElementById("todo-list");
+    const liEle = document.createElement("li");
+    liEle.classList.add("list-group-item", "p-3");
+    liEle.innerHTML = `
+    <input class="form-check-input me-1" id="todo-2" type="checkbox">
+    <label class="form-check-label" for="todo-2">
+    ${todo}
+    </label>
+    `;
+    ulEle.append(liEle);
+  }
+}
